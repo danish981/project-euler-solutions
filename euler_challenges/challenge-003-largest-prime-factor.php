d3d6532a8ae7c88f8849fadea59f276172e09745
Danish Mehmood <danishmehmood981@gmail.com>
2023-09-04 11:31:51 +0500

    Challenge #3 Incomplete-changes - test and get the next prime number added

--- euler_challenges/challenge-003-largest-prime-factor.php
+++ euler_challenges/challenge-003-largest-prime-factor.php
@@ -1,8 +1,26 @@
 <?php
 
-
 // the prime factors of 13195 are 5, 7, 13 and 29
 // what is the largest prime factor of the number 600851475143
 
+echo nextPrime(13);
 
+function nextPrime($prime) {
+    $nextPrime = $prime + 1;
+    while (!isPrime($nextPrime)) {
+        $nextPrime++;
+    }
+    return $nextPrime;
+}
 
+function isPrime($number) {
+    if ($number < 2) {
+        return false;
+    }
+    for ($i = 2; $i <= sqrt($number); $i++) {
+        if ($number % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
